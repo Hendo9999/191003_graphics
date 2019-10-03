39ac4a509355b4a9a6570dfc5ade19c9a3fb4b9b
Henderson <hen232@csiro.au>
2019-10-03 10:10:37 +0930

    commence script

---
+++ scripts/191003_graphics.R
@@ -0,0 +1,9 @@
+library(tidyverse) #load tidyverse package
+
+
+#Make a new project for this lesson and initialise a git repository.
+#Add the gapminder data from this link (or from your previous R lesson directory) to a data folder in your new repository.
+#Commit the data to the git repository.
+#Open a new script and use read_csv to read in the gapminder data and assign it to a variable called gapminder. (Hint: read_csv can be accessed through the tidyverse package).
+#Because our example figure uses only the data from 1977, create a new data frame called gapminder_1977 and assign it the data just from 1977 using the filter() function.
+#Commit the script to the repository
